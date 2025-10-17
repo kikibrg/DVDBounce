4202a6fb843d6f1a8e913c7675d41c351c4d8107
Kilian <wishii64@outlook.fr>
2025-10-17 20:53:00 +0200

    Create a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
 
 int main()
 {
+    RenderWindow window(VideoMode(1080, 720), "DVD");
     
+    while(window.isOpen() && !Keyboard::isKeyPressed(Keyboard::Escape))
+    {
+        Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
     return 0;
 }
