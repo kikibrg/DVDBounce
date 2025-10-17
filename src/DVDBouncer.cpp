b46831369efb9abc2a857ded9a67858d0fe4047a
Kilian <wishii64@outlook.fr>
2025-10-17 22:15:24 +0200

    Add random color change when hitting a border and changed fixed path to ressource folder

--- src/DVDBouncer.cpp
+++ src/DVDBouncer.cpp
@@ -1,47 +1,66 @@
 #include "DVDBouncer.hpp"
 #include <iostream>
-
+#include <filesystem>
 
 DVDBouncer::DVDBouncer()
 {
-    x = rand()%300;
-    y = rand()%300;
-    xspeed = 3;
-    yspeed = 3;
-    if(!texture.loadFromFile("C:\\Users\\Kilian\\Documents\\Code_projects\\DVDBounce\\resources\\dvd_logo.png"))
+    x = rand()%(980 - texture.getSize().x);
+    y = rand()%(620 - texture.getSize().y);;
+    xspeed = 4;
+    yspeed = 4;
+
+    std::filesystem::path imagePath = std::filesystem::current_path().parent_path().parent_path();
+    imagePath = imagePath / "resources" / "dvd_logo.png";
+
+    if(!texture.loadFromFile(imagePath.string()))
         std::cout << "Image not loading" << std::endl;
     sprite.setTexture(texture);
     sprite.setPosition(x,y);
+    setColor();
 }
+
 void DVDBouncer::moveSprite()
 {
     x += xspeed;
     y += yspeed;
 }
+
 void DVDBouncer::checkBoundaries(sf::Vector2i WindowCoordinate)
 {
     if(x + texture.getSize().x >= WindowCoordinate.x)
     {
         xspeed = -xspeed;
+        setColor();
     }
     if(x <= 0)
     {
         xspeed = -xspeed;
+        setColor();
     }
     if(y + texture.getSize().y >= WindowCoordinate.y)
     {
         yspeed = -yspeed;
+        setColor();
     }
     if(y <= 0)
     {
         yspeed = -yspeed;
+        setColor();
     }
 }
+
 void DVDBouncer::display()
 {
     sprite.setPosition(x,y);
 }
+
 sf::Sprite& DVDBouncer::draw()
 {
     return sprite;
+}
+
+void DVDBouncer::setColor()
+{
+    int colorIndex = rand()%10;
+    sprite.setColor(colorArray[colorIndex]);
 }
--- src/DVDBouncer.hpp
+++ src/DVDBouncer.hpp
@@ -12,12 +12,26 @@ class DVDBouncer
         float yspeed;
         sf::Texture texture;
         sf::Sprite sprite;
+        //White, Yellow, Red, Orange, Magenta, Purple, Cyan, Blue, Light green, Green
+        sf::Color colorArray[10] = {
+            sf::Color(255, 255, 255),
+            sf::Color(255, 255, 0),
+            sf::Color(255, 0, 0),
+            sf::Color(255, 165, 0),
+            sf::Color(255, 0, 255),
+            sf::Color(128, 0, 128),
+            sf::Color(0, 255, 255),
+            sf::Color(0, 0, 255),
+            sf::Color(0, 255, 0),
+            sf::Color(0, 128, 0)
+        };
     public:
         DVDBouncer();
         void moveSprite();
         void checkBoundaries(sf::Vector2i);
         void display();
         sf::Sprite& draw();
+        void setColor();
 };
 
 #endif
